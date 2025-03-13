890286aa4f9f100b6f31bd88b405c3812d6bbb80
vinaykumardag1 <112471376+vinaykumardag1@users.noreply.github.com>
2025-03-13 14:47:19 +0530

    smal issue solved

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { Link,useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-
 
 const Navbar = () => {
   const [userdata, setUserdata] = useState(null);
   const userId = localStorage.getItem("userId");
-  const navigate=useNavigate()
+  const navigate = useNavigate();
 
   useEffect(() => {
     const userDetail = async () => {
@@ -22,33 +21,33 @@ const Navbar = () => {
 
     userDetail();
   }, [userId]); // Dependency to re-fetch if userId changes
-  const Logout=()=>{
-    localStorage.clear()
-    window.location.reload()
-    navigate("/")
-  }
+
+  const Logout = () => {
+    localStorage.clear();
+    navigate("/");
+    window.location.reload();
+  };
+
   return (
     <div className='flex justify-between w-full p-10'>
       <h1>BlogApp</h1>
       <div className='flex gap-4'>
-       
         {/* Show Login/Register only if user is NOT logged in */}
         {!userId && (
-          <>
+          <div>
             <Link to='/'>Home</Link>
             <Link to='/login'>Login</Link>
             <Link to='/register'>Register</Link>
-          </>
+          </div>
         )}
         {/* Show User Name if logged in */}
         {userdata && (
           <div>
-             <Link to='/'>Home</Link>
-           
-            {/* <span >Myblogs</span> */}
-            <Link to="/myblogs">My Blogs</Link>
-             <span className='px-2'>HI! {userdata.name}</span>
-            <button className='ml-3 px-4 cursor-pointer text-white bg-red-500 py-2 rounded-4xl' onClick={Logout}>Logout</button>
+            <Link to='/'>Home</Link>
+            <Link to="/myblogs" className='px-2'>My Blogs</Link>
+            <span className='px-2'>HI! {userdata.name}</span>
+            
+            <button className='ml-3 px-4 cursor-pointer text-white bg-red-500 py-2 rounded-xl' onClick={Logout}>Logout</button>
           </div>
         )}
       </div>
